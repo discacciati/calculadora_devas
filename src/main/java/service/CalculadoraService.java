0d4f90b566b261e9b1b1a2efd82ed530dc59afe5
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2023-03-17 21:17:14 -0300

    Initial commit

---
+++ src/main/java/service/CalculadoraService.java
@@ -0,0 +1,33 @@
+package service;
+
+import model.Quadrado;
+import model.Triangulo;
+
+public class CalculadoraService {
+
+    public int somar(int a, int b) {
+        return a+b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a-b;
+    }
+
+    public int dividir(int a, int b) {
+        return a/b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a*b;
+    }
+
+//    public ??? calcularArea() {
+//    }
+
+    // Caso seja de area igual, retorna null
+//    public Triangulo trianguloDeMenorArea(Triangulo triangulo1, Triangulo triangulo2) {
+//    }
+//    public Quadrado quadradoDeMenorArea(Quadrado quadrado1, Quadrado quadrado2) {
+//    }
+
+}
